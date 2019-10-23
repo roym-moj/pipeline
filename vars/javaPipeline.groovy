f1882fc9539b0317435a787cbb8d43cb5a988af1
Roy Morgan <royjohnmorgan@gmail.com>
2019-10-23 21:43:35 +0100

    fix

--- vars/javaPipeline.groovy
+++ vars/javaPipeline.groovy
@@ -54,41 +54,41 @@ def call(body) {
             }
             
 
-//            stage('Package') {
-//                steps {
-//                    sh './gradlew bootJar'
-//                    sh "docker build -f Dockerfile -t ${env.JOB_NAME} ."
-//                    sh "docker tag ${env.JOB_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
-//                }
-//            }
-//            
-//            stage('Publish') {
-//                steps {
-//                    //  THIS SHOULD ALL BE IN THE CODE AS A LOGIN METHOD
-//                    sh 'loginvar=$(aws ecr get-login --no-include-email --region us-west-2) && eval "$loginvar"'
-//                    //This should not be done, instead seperate repos
-//                    sh "aws ecr batch-delete-image --repository-name demo --image-ids imageTag=${env.JOB_NAME} --region us-west-2"
-//                    
-//                    sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
-//                }
-//            }
+            stage('Package') {
+                steps {
+                    sh './gradlew bootJar'
+                    sh "docker build -f Dockerfile -t ${env.JOB_NAME} ."
+                    sh "docker tag ${env.JOB_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
+                }
+            }
+            
+            stage('Publish') {
+                steps {
+                    //  THIS SHOULD ALL BE IN THE CODE AS A LOGIN METHOD
+                    sh 'loginvar=$(aws ecr get-login --no-include-email --region us-west-2) && eval "$loginvar"'
+                    //This should not be done, instead seperate repos
+                    sh "aws ecr batch-delete-image --repository-name demo --image-ids imageTag=${env.JOB_NAME} --region us-west-2"
+                    
+                    sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
+                }
+            }
             
             stage('Create Cluster') {
                 steps {
                     //config
-                    sh "/usr/local/bin/ecs-cli configure --region us-west-2 --cluster ${env.JOB_NAME} --default-launch-type FARGATE --config-name '${env.JOB_NAME}' --force"
+                    sh "/usr/local/bin/ecs-cli configure --region us-west-2 --cluster ${env.JOB_NAME} --default-launch-type FARGATE --config-name ${env.JOB_NAME}"
                     //create cluster
-                    sh "/usr/local/bin/ecs-cli up --capability-iam --size 1 --instance-type t2.small --launch-type EC2 --cluster-config '${env.JOB_NAME}' --force --region us-west-2"
+                    sh "/usr/local/bin/ecs-cli up --capability-iam --size 1 --instance-type t2.small --launch-type EC2 --cluster-config ${env.JOB_NAME} --force --region us-west-2"
                 }
             }
             
             stage('Deploy services') {
                 steps {
                     //deploy docker images -- MANIFEST. (TODO in proper code)
-                    sh 'aws ecs register-task-definition --network-mode host --family demo-app-1 --region us-west-2 --container-definitions "[{\"name\":\"demo-app-1\",\"image\":\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demo-app-1\",\"cpu\":256,\"memory\":512,\"essential\":true}]"'
-                    sh 'aws ecs run-task --cluster appnamehere --task-definition demo-app-1 --count 1  --region us-west-2'
-                    sh 'aws ecs register-task-definition --network-mode host --family demo-app-2 --region us-west-2 --container-definitions "[{\"name\":\"demo-app-2\",\"image\":\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demo-app-2\",\"cpu\":256,\"memory\":512,\"essential\":true}]"'
-                    sh 'aws ecs run-task --cluster appnamehere --task-definition demo-app-2 --count 1  --region us-west-2'
+                    sh 'aws ecs register-task-definition --network-mode host --family demoapp1 --region us-west-2 --container-definitions "[{\"name\":\"demoapp1\",\"image\":\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demoapp1\",\"cpu\":256,\"memory\":512,\"essential\":true}]"'
+                    sh 'aws ecs run-task --cluster appnamehere --task-definition demoapp1 --count 1  --region us-west-2'
+                    sh 'aws ecs register-task-definition --network-mode host --family demoapp2 --region us-west-2 --container-definitions "[{\"name\":\"demoapp2\",\"image\":\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demoapp2\",\"cpu\":256,\"memory\":512,\"essential\":true}]"'
+                    sh 'aws ecs run-task --cluster appnamehere --task-definition demoapp2 --count 1  --region us-west-2'
                 }
             }
 //config
