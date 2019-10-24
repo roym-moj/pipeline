defab01ef5e8af35902cbdb77b653e4f7f264174
Roy Morgan <royjohnmorgan@gmail.com>
2019-10-24 16:13:12 +0100

    fix

--- src/demo/environments/EnvironmentActions.groovy
+++ src/demo/environments/EnvironmentActions.groovy
@@ -2,13 +2,29 @@ package demo.environments
 
 class Actions {
 
-    public static String createEnv(Script script) {
+    public static String createEmptyEnviroment(Script script, String name) {
+
+        script.echo("Creating ${name}")
+
+        
+        
+        return "some env name"
+
+    }
+
+    public static String populateEnviroment(Script script) {
 
         script.echo("Getting Env")
 
         return "some env name"
 
     }
-
-
+    
+    public static String deploySpecificService(Script script) {
+        //TODO
+    }
+    
+    public static String getPooledEnviroment(Script script) {
+        //TODO
+    }
 }
