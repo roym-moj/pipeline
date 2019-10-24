56c8fefc9630b51cb7fd6090fa02ddb2ba978621
Roy Morgan <royjohnmorgan@gmail.com>
2019-10-24 20:10:52 +0100

    prep

--- vars/javaPipeline.groovy
+++ vars/javaPipeline.groovy
@@ -35,14 +35,14 @@ def call(body) {
                 }
             }
 
-            stage('Checkstyle') {
-                steps {
-                    //OPTIONAL - FAILURE
-                        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                        sh './gradlew checkstyleMain'
-                    }
-                }
-            }
+//            stage('Checkstyle') {
+//                steps {
+//                    //OPTIONAL - FAILURE
+//                        catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+//                        sh './gradlew checkstyleMain'
+//                    }
+//                }
+//            }
 
             stage('Unit Test') {
                 steps {
@@ -55,65 +55,65 @@ def call(body) {
                 }
             }
 
-            stage('Package') {
-                steps {
-                    sh './gradlew bootJar'
-                    sh "docker build -f Dockerfile -t ${env.JOB_NAME} ."
-                    sh "docker tag ${env.JOB_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
-                }
-            }
-            
-            stage('Publish') {
-                steps {
-                    
-                    // AwsActions.logIn()
-                    
-                    sh 'loginvar=$(aws ecr get-login --no-include-email --region us-west-2) && eval "$loginvar"'
-                    sh "aws ecr batch-delete-image --repository-name demo --image-ids imageTag=${env.JOB_NAME} --region us-west-2"
-                    
-                    sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
-                }
-            }
-            
-            stage('Create/Update Enviroment') {
-                when {
-                    expression {
-                        return env.JOB_NAME == "demoapp1"
-                    }
-                }
-                steps {
-                    //EnviromentActions.createEmptyEnviroment(env.JOB_NAME, this)
-                    //OR                    
-                    //EnviromentActions.createEmptyEnviroment(env.JOB_NAME, this)
-                    sh "/usr/local/bin/ecs-cli up --capability-iam --size 1 --instance-type t3.medium --launch-type EC2 --cluster-config probationbuilds --region us-west-2 --force"
-                    sleep(60)
-                }
-
-            }
-            
-            stage('Deploy/Update services') {
-                steps {
-                    
-                    //Should use a manifest (embedded json file) and below methods
-                    //EnviromentActions.populateEnviroment(env.JOB_NAME, this)
-                    //EnviromentActions.deploySpecificService(env.JOB_NAME, this)
-                    sh "aws ecs register-task-definition --network-mode none --family ${env.JOB_NAME}" + ' --region us-west-2 --container-definitions "[{\\"name\\":\\"demoapp1\\",\\"image\\":\\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demoapp1\\",\\"cpu\\":512,\\"memory\\":512,\\"essential\\":true}, {\\"name\\":\\"demoapp2\\",\\"image\\":\\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demoapp2\\",\\"cpu\\":512,\\"memory\\":512,\\"essential\\":true}]"'
-                    sh "aws ecs run-task --cluster probationbuilds --task-definition ${env.JOB_NAME} --count 1  --region us-west-2"
-                }
-            }        
-            
-            stage('Run Global Tests for probation') {
-                steps {
-                    sleep(30)
-                    echo "Running Tests"
-                }
-            }    
-            
-            stage('Return/Destroy Enviroment') {
-                steps {
-                    echo "Destroy Manually!!! for now"
-                }
-            }
+//            stage('Package') {
+//                steps {
+//                    sh './gradlew bootJar'
+//                    sh "docker build -f Dockerfile -t ${env.JOB_NAME} ."
+//                    sh "docker tag ${env.JOB_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
+//                }
+//            }
+//            
+//            stage('Publish') {
+//                steps {
+//                    
+//                    // AwsActions.logIn()
+//                    
+//                    sh 'loginvar=$(aws ecr get-login --no-include-email --region us-west-2) && eval "$loginvar"'
+//                    sh "aws ecr batch-delete-image --repository-name demo --image-ids imageTag=${env.JOB_NAME} --region us-west-2"
+//                    
+//                    sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-west-2.amazonaws.com/demo:${env.JOB_NAME}"
+//                }
+//            }
+//            
+//            stage('Create/Update Enviroment') {
+//                when {
+//                    expression {
+//                        return env.JOB_NAME == "demoapp1"
+//                    }
+//                }
+//                steps {
+//                    //EnviromentActions.createEmptyEnviroment(env.JOB_NAME, this)
+//                    //OR                    
+//                    //EnviromentActions.createEmptyEnviroment(env.JOB_NAME, this)
+//                    sh "/usr/local/bin/ecs-cli up --capability-iam --size 1 --instance-type t3.medium --launch-type EC2 --cluster-config probationbuilds --region us-west-2 --force"
+//                    sleep(60)
+//                }
+//
+//            }
+//            
+//            stage('Deploy/Update services') {
+//                steps {
+//                    
+//                    //Should use a manifest (embedded json file) and below methods
+//                    //EnviromentActions.populateEnviroment(env.JOB_NAME, this)
+//                    //EnviromentActions.deploySpecificService(env.JOB_NAME, this)
+//                    sh "aws ecs register-task-definition --network-mode none --family ${env.JOB_NAME}" + ' --region us-west-2 --container-definitions "[{\\"name\\":\\"demoapp1\\",\\"image\\":\\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demoapp1\\",\\"cpu\\":512,\\"memory\\":512,\\"essential\\":true}, {\\"name\\":\\"demoapp2\\",\\"image\\":\\"651524873607.dkr.ecr.us-west-2.amazonaws.com/demo:demoapp2\\",\\"cpu\\":512,\\"memory\\":512,\\"essential\\":true}]"'
+//                    sh "aws ecs run-task --cluster probationbuilds --task-definition ${env.JOB_NAME} --count 1  --region us-west-2"
+//                }
+//            }        
+//            
+//            stage('Run Global Tests for probation') {
+//                steps {
+//                    sleep(30)
+//                    echo "Running Tests"
+//                }
+//            }    
+//            
+//            stage('Return/Destroy Enviroment') {
+//                steps {
+//                    echo "Destroy Manually!!! for now"
+//                }
+//            }
         }
     }
 }
